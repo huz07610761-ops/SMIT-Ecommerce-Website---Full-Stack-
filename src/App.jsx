ecb18c2336f7e23ed1a25cea0c3c2051722481b3
Huzaifa Asif <hasnainasif07610761@gmail.com>
2026-08-17 08:20:32 +0500

    Add Product Detail Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HeroSection from './components/heroSection/HeroSection'
 import Website_MainPage from './pages/Website_MainPage'
 import Website_Category_Page from './pages/Website_Category_Page'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Product_Details_Page from './pages/Product_Details_Page'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Website_MainPage/>} />
         <Route path='/category' element={<Website_Category_Page/>} />
+        <Route path='/productDetails' element={<Product_Details_Page/>} />
       </Routes>
       </BrowserRouter>
   )
---
+++ src/pages/Product_Details_Page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AnoucementBar from '../components/annoucement bar/AnoucementBar'
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
+
+function Product_Details_Page() {
+  return (
+    <div>
+      <AnoucementBar/>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Product_Details_Page
